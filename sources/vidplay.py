d893975d5fef26dda109b54bcd1588b94e80dabb
Ciarands <ravenreju@gmail.com>
2024-01-05 08:24:43 +0000

    Update vidplay.py

--- sources/vidplay.py
+++ sources/vidplay.py
@@ -4,7 +4,7 @@ import requests
 
 from urllib.parse import unquote
 from typing import Optional, Tuple, Dict, List
-from utils import Utilities, CouldntFetchKeysError
+from utils import Utilities, CouldntFetchKeys
 
 class VidplayExtractor:
     KEY_URL : str = "https://raw.githubusercontent.com/Claudemirovsky/worstsource-keys/keys/keys.json" # love u claude
@@ -31,7 +31,7 @@ class VidplayExtractor:
         fetch_keys = requests.get(key_url)
 
         if fetch_keys.status_code != 200:
-            raise CouldntFetchKeysError("Failed to fetch decryption keys!")
+            raise CouldntFetchKeys("Failed to fetch decryption keys!")
 
         key1, key2 = fetch_keys.json()
         decoded_id = Utilities.decode_data(key1, v_id)
@@ -44,21 +44,20 @@ class VidplayExtractor:
     
     def resolve_source(self, url: str, fetch_subtitles: bool, provider_url: str) -> Tuple[Optional[List], Optional[Dict]]:
         url_data = url.split("?")
+
         subtitles = {}
         if fetch_subtitles:
             subtitles = self.get_vidplay_subtitles(url_data[1])
 
         key = self.encode_id(self.KEY_URL, url_data[0].split("/e/")[-1])
         futoken = self.get_futoken(key, url, provider_url)
-
         
         req = requests.get(f"{provider_url}/mediainfo/{futoken}?{url_data[1]}&autostart=true", headers={"Referer": url})
         if req.status_code != 200:
-            print(f"[Vidplay] Failed to retrieve media, status code: {req.status_code}...")
+            print(f"[VidplayExtractor] Failed to retrieve media, status code: {req.status_code}...")
             return None, None
 
         req_data = req.json()
-
         if (req_data.get("result")) and (type(req_data.get("result")) == dict):
             sources = req_data.get("result").get("sources")
             return [value.get("file") for value in sources], subtitles
