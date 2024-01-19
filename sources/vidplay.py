109b3f7b05ac5aff0739ca89fefb57319a4c3de4
Ciarán <74070993+Ciarands@users.noreply.github.com>
2024-01-19 13:08:00 +0000

    Update vidplay.py

--- sources/vidplay.py
+++ sources/vidplay.py
@@ -7,7 +7,7 @@ from typing import Optional, Tuple, Dict, List
 from utils import Utilities, CouldntFetchKeys
 
 class VidplayExtractor:
-    KEY_URL : str = "https://raw.githubusercontent.com/Claudemirovsky/worstsource-keys/keys/keys.json" # love u claude
+    KEY_URL : str = "https://raw.githubusercontent.com/Ciarands/worstsource-keys/keys/keys.json" # love u claude
 
     @staticmethod
     def get_futoken(key: str, url: str, provider_url: str) -> str:
