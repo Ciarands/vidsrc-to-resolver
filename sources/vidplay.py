6467e0bf932c6904276fb66f28cc948f77f457ed
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-04-07 17:58:39 +0100

    Fix vidplay

--- sources/vidplay.py
+++ sources/vidplay.py
@@ -11,7 +11,7 @@ class VidplayExtractor:
 
     @staticmethod
     def get_futoken(key: str, url: str, provider_url: str) -> str:
-        req = requests.get(f"{provider_url}/futoken", {"Referer": url})
+        req = requests.get(f"{provider_url}/futoken", headers={"Referer": url})
         fu_key = re.search(r"var\s+k\s*=\s*'([^']+)'", req.text).group(1)
         
         return f"{fu_key},{','.join([str(ord(fu_key[i % len(fu_key)]) + ord(key[i])) for i in range(len(key))])}"
